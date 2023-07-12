ecc171881a34d91a47be1c239961d9c946ceb97f
Sean <pca.sean.jeun@gmail.com>
2023-07-12 16:33:02 -0400

    mergesort method built

--- practice-for-ch-ruby-recursion-long-practice-main/Recursion.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/Recursion.rb
@@ -79,3 +79,24 @@ def bsearch(array, target)
         right_value == nil ? nil :  right_value + mid + 1
     end
 end
+
+def merge_sort(arr)
+    return arr if arr.size <= 1
+    mid = arr.size / 2
+    left = merge_sort(arr[0...mid])
+    right = merge_sort(arr[mid...arr.size])
+    merge(left, right)
+end
+
+def merge(left, right)
+    result = []
+    until left.empty? || right.empty?
+        if left.first < right.first
+            result << left.shift
+        else
+            result << right.shift
+        end
+    end
+
+    result + left + right
+end
